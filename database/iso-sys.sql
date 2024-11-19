50e498c21b9c5c9c91722aef10690cd524cab27d
kakolukiyan <daniel.alfonsibusiness@gmail.com>
2024-11-18 21:48:59 -0400

    [DATABASE]

--- database/iso-sys.sql
+++ database/iso-sys.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 07-11-2024 a las 07:05:29
+-- Tiempo de generación: 19-11-2024 a las 02:48:39
 -- Versión del servidor: 10.4.32-MariaDB
 -- Versión de PHP: 8.2.12
 
@@ -42,9 +42,10 @@ CREATE TABLE `exams` (
 --
 
 INSERT INTO `exams` (`id`, `unit_id`, `title`, `description`, `exam_order`, `total_score`, `isDeleted`) VALUES
-(1, 2, 'variables en c++', 'tipos de variables en c++', 0, 10, 0),
-(2, 4, 'titulo 1', 'this is a description', 0, 21, 0),
-(3, 3, 'pepesss', 'sdsadsdadasdas', 0, 11, 1);
+(1, 2, 'variables en c++', 'tipos de variables en c++', 1, 20, 1),
+(2, 4, 'titulo 1', 'this is a description', 1, 20, 0),
+(3, 3, 'pepesss', 'sdsadsdadasdas', 1, 11, 1),
+(4, 7, 'examen prueba', 'examen test', 0, 20, 0);
 
 -- --------------------------------------------------------
 
@@ -58,6 +59,14 @@ CREATE TABLE `exam_scores` (
   `exam_id` int(11) DEFAULT NULL,
   `score` decimal(5,2) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `exam_scores`
+--
+
+INSERT INTO `exam_scores` (`id`, `user_id`, `exam_id`, `score`) VALUES
+(1, 3, 2, 13.00),
+(2, 3, 4, 0.00);
 
 -- --------------------------------------------------------
 
@@ -89,7 +98,9 @@ INSERT INTO `guides` (`id`, `lesson_id`, `file`, `name`) VALUES
 (47, 16, 'guides/document (1).pdf', 'document (1).pdf'),
 (48, 5, 'guides/Unidades.pdf', 'Unidades.pdf'),
 (49, 15, 'guides/Captura de pantalla_5-10-2024_193430_copilot.microsoft.com.jpeg', 'Captura de pantalla_5-10-2024_193430_copilot.microsoft.com.jpeg'),
-(50, 15, 'guides/Captura de pantalla_4-10-2024_105932_bdvenlinea.banvenez.com.jpeg', 'Captura de pantalla_4-10-2024_105932_bdvenlinea.banvenez.com.jpeg');
+(50, 15, 'guides/Captura de pantalla_4-10-2024_105932_bdvenlinea.banvenez.com.jpeg', 'Captura de pantalla_4-10-2024_105932_bdvenlinea.banvenez.com.jpeg'),
+(51, 6, 'guides/Solicitud_titulo_21_11188.pdf', 'Solicitud_titulo_21_11188.pdf'),
+(52, 6, 'guides/Captura de pantalla_10-11-2024_91425_bdvenlinea.banvenez.com.jpeg', 'Captura de pantalla_10-11-2024_91425_bdvenlinea.banvenez.com.jpeg');
 
 -- --------------------------------------------------------
 
@@ -104,28 +115,33 @@ CREATE TABLE `lessons` (
   `content` text DEFAULT NULL,
   `lesson_order` int(100) NOT NULL,
   `summary` text NOT NULL,
-  `url` varchar(1000) NOT NULL
+  `url` varchar(1000) NOT NULL,
+  `isDeleted` int(11) NOT NULL DEFAULT 0
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Volcado de datos para la tabla `lessons`
 --
 
-INSERT INTO `lessons` (`id`, `unit_id`, `title`, `content`, `lesson_order`, `summary`, `url`) VALUES
-(3, 3, 'unidad 13: suma y resta fracciones (diferentes denominadores)', '', 2, 'esto es un summary', 'sadasd'),
-(4, 3, 'el puntos', NULL, 3, 'esta es una prueba', 'https://youtu.be/leUgu-6bWzY'),
-(5, 4, 'el enfoque sistematico', NULL, 2, 'asdasd', 'https://youtu.be/leUgu-6bWzY'),
-(6, 4, 'introducción a los sistemas', NULL, 1, 'this is a description', 'https://youtu.be/leUgu-6bWzY'),
-(7, 3, 'cuarta ', NULL, 4, 'this is a summary', 'asdasdas'),
-(8, 7, 'titulo leccion 1', NULL, 1, 'esto es un texto', 'https://youtu.be/leUgu-6bWzY'),
-(9, 8, 'sadasdas', NULL, 1, 'asdasdas', 'dasdasdasd'),
-(10, 8, 'asdasd', NULL, 2, 'asdasdasd', 'asdasdasd'),
-(11, 8, 'asdsadas', NULL, 22, 'asdasd', 'asdasdasd'),
-(12, 8, 'asasd', NULL, 5, 'asdasdasd', 'asdasdas'),
-(13, 8, 'asdasd', NULL, 6, 'asdasdasda', 'asdasd'),
-(14, 4, 'sistemas de informacion', NULL, 3, 'esto es una prueba', 'https://youtu.be/leUgu-6bWzY'),
-(15, 4, 'tipos de sistemas de información', NULL, 4, 'this is a test', 'https://youtu.be/leUgu-6bWzY'),
-(16, 4, 'ciclo de vida del desarrollo del sistema', NULL, 5, 'this is a test', 'https://youtu.be/leUgu-6bWzY');
+INSERT INTO `lessons` (`id`, `unit_id`, `title`, `content`, `lesson_order`, `summary`, `url`, `isDeleted`) VALUES
+(3, 3, 'unidad 13: suma y resta fracciones (diferentes denominadores)', '', 2, 'esto es un summary', 'sadasd', 0),
+(4, 3, 'el puntos', NULL, 3, 'esta es una prueba', 'https://youtu.be/leUgu-6bWzY', 0),
+(5, 4, 'el enfoque sistematico', NULL, 2, 'asdasd', 'test.mp4', 0),
+(6, 4, 'introducción a los sistemas', NULL, 1, 'this is a description', 'https://youtu.be/leUgu-6bWzY', 0),
+(7, 3, 'cuarta ', NULL, 4, 'this is a summary', 'asdasdas', 0),
+(8, 7, 'titulo leccion 1', NULL, 1, 'esto es un texto', 'https://youtu.be/leUgu-6bWzY', 0),
+(9, 8, 'sadasdas', NULL, 1, 'asdasdas', 'dasdasdasd', 0),
+(10, 8, 'asdasd', NULL, 2, 'asdasdasd', 'asdasdasd', 0),
+(11, 8, 'asdsadas', NULL, 22, 'asdasd', 'asdasdasd', 0),
+(12, 8, 'asasd', NULL, 5, 'asdasdasd', 'asdasdas', 0),
+(13, 8, 'asdasd', NULL, 6, 'asdasdasda', 'asdasd', 0),
+(14, 4, 'sistemas de informacion', NULL, 3, 'esto es una prueba', 'https://youtu.be/leUgu-6bWzY', 0),
+(15, 4, 'tipos de sistemas de información', NULL, 4, 'this is a test', 'https://youtu.be/leUgu-6bWzY', 0),
+(16, 4, 'ciclo de vida del desarrollo del sistema', NULL, 5, 'this is a testa', 'edit_test.mp4', 0),
+(17, 4, 'asdasdas', NULL, 6, 'dasdasdd', 'asdasdas', 0),
+(18, 4, 'asdasd', NULL, 15, 'asdasdasd', 'asdasdasd', 1),
+(19, 4, 'aaaaaaa', NULL, 10, 'asdasda', 'test.mp4', 1),
+(20, 4, 'titulo 111', NULL, 11, 'awanile', 'test.mp4', 0);
 
 -- --------------------------------------------------------
 
@@ -156,7 +172,7 @@ INSERT INTO `person` (`id`, `nationality`, `cedula`, `name`, `second_name`, `las
 (18, '', '', 'asd', '', 'das', '', '', '2024-10-18', '', ''),
 (19, '', '', 'asd', '', 'asd', '', '', '2024-10-07', '', ''),
 (20, '', '', 'asd', '', 'asd', '', '', '2024-10-15', '', ''),
-(21, '', '', 'admin', '', 'admin', '', '', '2024-10-15', '', ''),
+(21, 'V-', '28129644', 'admin', 'queso', 'admin', 'alfonsi', '04128581138', '2024-10-15', 'masculino', 'no se '),
 (22, '', '', 'iran', '', 'indriago', '', '', '2024-10-09', '', ''),
 (23, 'V-', '28129366', 'iran', 'andres', 'indrigo', 'raul', '04128581138', '2006-01-01', '', 'calle juncal'),
 (24, 'V-', '4949895', 'mercedes', '', 'figuera', '', '04128146555', '2006-01-01', '', 'no se'),
@@ -169,7 +185,11 @@ INSERT INTO `person` (`id`, `nationality`, `cedula`, `name`, `second_name`, `las
 (31, '', '', 'nmkj', '', 'jhjhj', '', '', '2024-10-10', '', ''),
 (32, '', '', 'nmkj', '', 'jhjhj', '', '', '2024-10-10', '', ''),
 (33, '', '', 'asd', '', 'asd', '', '', '2024-10-21', '', ''),
-(34, '', '', 'asd', '', 'asd', '', '', '2024-11-14', '', '');
+(34, '', '', 'asd', '', 'asd', '', '', '2024-11-14', '', ''),
+(35, 'V-', '4949895', 'meche', '', 'figuera', '', '04128844454', '1992-10-14', '', 'no se'),
+(36, 'V-', '4949894', 'pierry', '', 'moya', '', '04148581138', '2005-12-05', '', 'no se '),
+(37, 'V-', '28128555', 'queso', '', 'alfonsi', '', '', '2024-11-11', '', ''),
+(38, 'V-', '3498555', 'lourdes', '', 'figuera', '', '04128581138', '2006-01-01', '', 'no lo se');
 
 -- --------------------------------------------------------
 
@@ -181,21 +201,22 @@ CREATE TABLE `questions` (
   `id` int(11) NOT NULL,
   `exam_id` int(11) NOT NULL,
   `text` text NOT NULL,
-  `type` varchar(100) NOT NULL DEFAULT 'SIMPLE',
   `question_order` int(11) NOT NULL,
-  `question_mark` int(11) NOT NULL,
-  `complete_answer` text NOT NULL,
-  `simple_answer` int(11) NOT NULL
+  `question_mark` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Volcado de datos para la tabla `questions`
 --
 
-INSERT INTO `questions` (`id`, `exam_id`, `text`, `type`, `question_order`, `question_mark`, `complete_answer`, `simple_answer`) VALUES
-(1, 1, 'como se declaran las variables enteras', '', 1, 1, '', 0),
-(8, 1, 'cual es la sintaxis de un \"if\"', '', 2, 2, '', 0),
-(9, 1, 'como se hace un \"for\"', '', 3, 3, '', 0);
+INSERT INTO `questions` (`id`, `exam_id`, `text`, `question_order`, `question_mark`) VALUES
+(1, 3, 'como se declaran las variables enteras', 1, 1),
+(8, 3, 'cual es la sintaxis de un \"if\"', 2, 2),
+(38, 2, 'pregunta de prueba 1', 1, 4),
+(39, 2, 'pregunta de prueba 2', 2, 2),
+(40, 2, 'pregunta de prueba 3', 3, 2),
+(41, 2, 'prueba 3', 4, 5),
+(42, 4, 'pregunta para terminar\n', 1, 20);
 
 -- --------------------------------------------------------
 
@@ -219,9 +240,21 @@ CREATE TABLE `questions_data` (
 INSERT INTO `questions_data` (`id`, `question_id`, `exam_id`, `answer`, `type`, `true_response`) VALUES
 (82, 9, 1, 'ss', 'radius', 'true'),
 (83, 9, 1, 'ssasdasdasdasd sadadadasdadasd as das da asasdasd as as asdaasdasdas', 'radius', 'false'),
-(129, 1, 1, 'cxcxcx', 'radius', 'true'),
 (131, 8, 1, 'cxcxcx', 'radius', 'false'),
-(132, 8, 1, 'cxcxcx', 'radius', 'false');
+(132, 8, 1, 'cxcxcx', 'radius', 'false'),
+(134, 38, 2, 'es esta pregunta verdadera?', 'radius', 'true'),
+(135, 38, 2, 'es esta pregunta falsa?', 'radius', 'false'),
+(138, 39, 2, 'opcion multiple 2', 'checkbox', 'false'),
+(139, 39, 2, 'opcion multiple 3', 'checkbox', 'false'),
+(140, 39, 2, 'opcion multiple 4', 'checkbox', 'false'),
+(141, 39, 2, 'opcion multiple 5', 'checkbox', 'true'),
+(142, 39, 2, 'opcion multiple 1', 'checkbox', 'true'),
+(145, 41, 2, 'checkbox 1', 'checkbox', 'true'),
+(146, 41, 2, 'checkbox 2', 'checkbox', 'false'),
+(147, 41, 2, 'checkbox 3', 'checkbox', 'false'),
+(150, 40, 2, 'las variables en php se declaran con el simbolo:', 'text', '$'),
+(151, 42, 4, 'this', 'radius', 'true'),
+(152, 42, 4, 'also this', 'radius', 'false');
 
 -- --------------------------------------------------------
 
@@ -262,12 +295,13 @@ CREATE TABLE `units` (
 
 INSERT INTO `units` (`id`, `subject_id`, `name`, `unit_order`, `isDeleted`) VALUES
 (2, 1, 'asdasd', 3, 1),
-(3, 1, 'asdasd', 3, 0),
+(3, 1, 'nuevo nombre', 3, 0),
 (4, 1, 'fundamentos de sistemas', 1, 0),
 (5, 1, 'prueba 24/10/24', 7, 1),
 (6, 1, 'prueba 2', 2, 1),
 (7, 1, 'unidad 2', 2, 0),
-(8, 1, 'asdasdasd', 5, 1);
+(8, 1, 'asdasdasd', 5, 1),
+(9, 1, 'prueba 4', 4, 1);
 
 -- --------------------------------------------------------
 
@@ -282,23 +316,28 @@ CREATE TABLE `user` (
   `isAdmin` int(10) NOT NULL,
   `email` varchar(100) NOT NULL,
   `isBlocked` int(11) NOT NULL DEFAULT 0,
-  `isDeleted` int(11) NOT NULL DEFAULT 0
+  `isDeleted` int(11) NOT NULL DEFAULT 0,
+  `date` date DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Volcado de datos para la tabla `user`
 --
 
-INSERT INTO `user` (`user_id`, `person_id`, `password`, `isAdmin`, `email`, `isBlocked`, `isDeleted`) VALUES
-(2, 20, '$2y$10$fkimP72pdZrdjLSdKZiMyefGOlTi3jk/ajsXHGLPJwYR0vgA46MSO', 1, 'daniel.alfonsi2011@gmail.com', 0, 0),
-(3, 21, '$2y$10$j.GDCzymMvrbP6kMszGQ6OOTL4ILxO5UeR9ZPAAxkJRR4vK8wrwn6', 1, 'admin@gmail.com', 0, 0),
-(4, 22, '$2y$10$pjuqi247hSmCAN.7PE9dF.s.8sq0V9ni6uE4QGMcWuhWEam/qc.d2', 0, 'IranIndriago@gmail.com', 0, 0),
-(5, 23, '$2y$10$BFFOIX7HgH0QpQZ41JpbQOlSR2SpHOU2eAQlspO16L/ob9hGslYIW', 1, 'irania@gmail.com', 0, 0),
-(6, 24, '$2y$10$fbBXh8.wLy0x1IBuVnXCP.pxxzrrd4wkaVunnHfwE5wL7/qYdmCky', 0, 'mechemeche@gmail.com', 0, 0),
-(7, 25, '$2y$10$3pJbTd/jBXayzHX0KxpzNe6y4RE8i2pkJxo28sH6xBV/SBzfq7Zoa', 0, 'asda@gmail.com', 0, 0),
-(8, 26, '$2y$10$JpkX3IWW61yAoqH4RjIDj.QFJJFxW0qVQXp9aGuweZsr/NUPFoCbq', 0, 'IranIndriagos@gmail.com', 0, 0),
-(9, 33, '$2y$10$DQZ1dUcLk1I13jhtJFtUUeE2dFIB2ixap7Cp91q3lq2QKdokxGAFG', 0, 'IranIndriagoss@gmail.com', 1, 0),
-(10, 34, '$2y$10$TPh1KxEF3kVxGFWqM1kYOe0IQmYg0bb8EX.FTVmy5F6S2IrHtWYzG', 0, 'Ray@gmail.com', 0, 0);
+INSERT INTO `user` (`user_id`, `person_id`, `password`, `isAdmin`, `email`, `isBlocked`, `isDeleted`, `date`) VALUES
+(2, 20, '$2y$10$fkimP72pdZrdjLSdKZiMyefGOlTi3jk/ajsXHGLPJwYR0vgA46MSO', 1, 'daniel.alfonsi2011@gmail.com', 0, 0, '2024-10-28'),
+(3, 21, '$2y$10$fbBXh8.wLy0x1IBuVnXCP.pxxzrrd4wkaVunnHfwE5wL7/qYdmCky', 1, 'admin@gmail.com', 0, 0, '2024-11-11'),
+(4, 22, '$2y$10$8WYYzQQsIIGhV/xVgKoKA.cvQSAjkD.QDbKPgRpe2lHmZcvis.tCa', 1, 'IranIndriago@gmail.com', 0, 1, '2024-05-15'),
+(5, 23, '$2y$10$j/O.U.rH1.r39XLXivJDN.IZA8oABgfwTvz5LxWFl0U0WUNkNNRgC', 0, 'irania@gmail.com', 0, 0, '2024-07-29'),
+(6, 24, '$2y$10$fbBXh8.wLy0x1IBuVnXCP.pxxzrrd4wkaVunnHfwE5wL7/qYdmCky', 0, 'mechemeche@gmail.com', 0, 0, '2024-04-08'),
+(7, 25, '$2y$10$3pJbTd/jBXayzHX0KxpzNe6y4RE8i2pkJxo28sH6xBV/SBzfq7Zoa', 0, 'asda@gmail.com', 0, 1, '2024-08-18'),
+(8, 26, '$2y$10$JpkX3IWW61yAoqH4RjIDj.QFJJFxW0qVQXp9aGuweZsr/NUPFoCbq', 0, 'IranIndriagos@gmail.com', 0, 0, '2024-08-11'),
+(9, 33, '$2y$10$DQZ1dUcLk1I13jhtJFtUUeE2dFIB2ixap7Cp91q3lq2QKdokxGAFG', 0, 'IranIndriagoss@gmail.com', 1, 1, '2024-07-15'),
+(10, 34, '$2y$10$TPh1KxEF3kVxGFWqM1kYOe0IQmYg0bb8EX.FTVmy5F6S2IrHtWYzG', 0, 'Ray@gmail.com', 0, 0, '2024-07-07'),
+(11, 35, '$2y$10$NZB.guHzmqd.UcOPi4AWru.ZX1dI3LOMakfvXUkOahGjMCIL2FEwq', 0, 'meme@gmail.com', 0, 0, '2024-09-09'),
+(12, 36, '$2y$10$9XnQVh4d11GWnZbyximyfem82yd8hw3w3WyV5uR1YGPz8S8XMwTxm', 0, 'Pierry@gmail.com', 0, 0, '2024-11-15'),
+(13, 37, '$2y$10$MH2R7LM088XFiq6yQr3GTugJfFudPu67nsF7Aea1YxzKFvzZTlv.q', 0, 'queso@gmail.com', 0, 0, '2024-11-15'),
+(14, 38, '$2y$10$Nj6ijB8Af27zgWm7ibHrd.ody8q.uXE5r2Jjxbx21cGTA5dDkOfbu', 0, 'lulu@gmail.com', 0, 0, '2024-11-18');
 
 -- --------------------------------------------------------
 
@@ -312,6 +351,59 @@ CREATE TABLE `user_history` (
   `action` varchar(255) NOT NULL,
   `date` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `user_history`
+--
+
+INSERT INTO `user_history` (`id`, `user_id`, `action`, `date`) VALUES
+(16, 3, 'admin ha editado una unidad', '2024-11-15 17:52:14'),
+(17, 3, 'admin ha añadido una unidad', '2024-11-15 17:57:20'),
+(18, 3, 'admin ha desabilitado una unidad', '2024-11-15 17:57:37'),
+(19, 3, 'admin ha desabilitado una Lección', '2024-11-18 04:08:52'),
+(20, 3, 'admin ha desabilitado una Lección', '2024-11-18 04:12:03'),
+(21, 3, 'admin ha creado una Lección', '2024-11-18 04:14:15'),
+(22, 3, 'admin ha editado una Lección', '2024-11-18 04:14:23'),
+(23, 3, 'admin ha editado una Lección', '2024-11-18 04:24:47'),
+(24, 3, 'admin ha editado un usuario', '2024-11-18 04:37:02'),
+(25, 3, 'admin ha editado un usuario', '2024-11-18 04:37:29'),
+(26, 3, 'admin ha borrado a un usuario', '2024-11-18 04:41:57'),
+(27, 3, 'admin ha bloqueado a un usuario', '2024-11-18 04:42:04'),
+(28, 3, 'admin ha bloqueado a un usuario', '2024-11-18 04:42:05'),
+(29, 3, 'admin ha borrado a un usuario', '2024-11-18 04:42:10'),
+(30, 3, 'admin ha borrado a un usuario', '2024-11-18 04:42:16'),
+(31, 3, 'admin ha desabilitado un Examen', '2024-11-18 13:34:14'),
+(32, 3, 'admin ha editado una Lección', '2024-11-18 18:32:33'),
+(33, 3, 'admin ha creado un examen', '2024-11-19 01:41:57');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `videos`
+--
+
+CREATE TABLE `videos` (
+  `id` int(11) NOT NULL,
+  `lesson_id` int(11) DEFAULT NULL,
+  `file` varchar(255) DEFAULT NULL,
+  `name` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `videos`
+--
+
+INSERT INTO `videos` (`id`, `lesson_id`, `file`, `name`) VALUES
+(1, 17, 'videos/test.mp4', ''),
+(2, 17, 'videos/test.mp4', ''),
+(3, 18, 'videos/test.mp4', ''),
+(4, 18, 'videos/test.mp4', ''),
+(5, 18, 'videos/test.mp4', ''),
+(6, 19, 'videos/test.mp4', 'test.mp4'),
+(7, 19, 'videos/edit_test.mp4', 'edit_test.mp4'),
+(8, 21, 'videos/test.mp4', 'test.mp4'),
+(9, 20, 'videos/test.mp4', 'test.mp4'),
+(10, 5, 'videos/edit_test.mp4', 'edit_test.mp4');
 
 --
 -- Índices para tablas volcadas
@@ -393,6 +485,13 @@ ALTER TABLE `user_history`
   ADD KEY `user_id` (`user_id`);
 
 --
+-- Indices de la tabla `videos`
+--
+ALTER TABLE `videos`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `lesson_id` (`lesson_id`);
+
+--
 -- AUTO_INCREMENT de las tablas volcadas
 --
 
@@ -400,43 +499,43 @@ ALTER TABLE `user_history`
 -- AUTO_INCREMENT de la tabla `exams`
 --
 ALTER TABLE `exams`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT de la tabla `exam_scores`
 --
 ALTER TABLE `exam_scores`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT de la tabla `guides`
 --
 ALTER TABLE `guides`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=51;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=54;
 
 --
 -- AUTO_INCREMENT de la tabla `lessons`
 --
 ALTER TABLE `lessons`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
 
 --
 -- AUTO_INCREMENT de la tabla `person`
 --
 ALTER TABLE `person`
-  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=35;
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=39;
 
 --
 -- AUTO_INCREMENT de la tabla `questions`
 --
 ALTER TABLE `questions`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=43;
 
 --
 -- AUTO_INCREMENT de la tabla `questions_data`
 --
 ALTER TABLE `questions_data`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=133;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=153;
 
 --
 -- AUTO_INCREMENT de la tabla `subjects`
@@ -448,19 +547,25 @@ ALTER TABLE `subjects`
 -- AUTO_INCREMENT de la tabla `units`
 --
 ALTER TABLE `units`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- AUTO_INCREMENT de la tabla `user`
 --
 ALTER TABLE `user`
-  MODIFY `user_id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `user_id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
 
 --
 -- AUTO_INCREMENT de la tabla `user_history`
 --
 ALTER TABLE `user_history`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=34;
+
+--
+-- AUTO_INCREMENT de la tabla `videos`
+--
+ALTER TABLE `videos`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
 -- Restricciones para tablas volcadas
